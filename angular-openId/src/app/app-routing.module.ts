58613f8b675c92016dc736b9c0fe9f564279a216
Pedro Romero <pedrojesusromeroortega@gmail.com>
2021-08-24 13:10:42 +0200

    feat: add routes

--- angular-openId/src/app/app-routing.module.ts
+++ angular-openId/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const appRoutes: Routes = [];
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+];
 
 @NgModule({
   imports: [
